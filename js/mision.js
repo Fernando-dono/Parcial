41014bfee6a221672165d98b6f182d20fe09597f
Fernando <feranpepo010805@gmail.com>
2025-03-11 07:59:48 -0500

    recuperacion

---
+++ js/mision.js
@@ -0,0 +1,9 @@
+function cambiarTamano() {
+    let texto = document.getElementById("texto");
+    texto.style.fontSize = texto.style.fontSize === "16px" ? "20px" : "16px";
+}
+
+function cambiarColor() {
+    let texto = document.getElementById("texto");
+    texto.style.color = texto.style.color === "black" ? "blue" : "black";
+}
